36fa699b5227a09a999fcf09a613f83506c8ab4c
Federico Bond <federicobond@gmail.com>
2017-06-15 01:05:13 -0300

    Fix test script

--- run-tests.sh
+++ run-tests.sh
@@ -10,10 +10,7 @@ if [ ! -e "$ANTLR_JAR" ]; then
   curl http://www.antlr.org/download/antlr-4.7-complete.jar -o "$ANTLR_JAR"
 fi
 
-alias antlr4="java -jar $ANTLR_JAR"
-alias grun="java -classpath $ANTLR_JAR:. org.antlr.v4.TestRig"
-
-antlr4 Solidity.g4
+java -jar $ANTLR_JAR Solidity.g4
 javac -classpath $ANTLR_JAR $GRAMMAR*.java
 
-grun "$GRAMMAR" "$START_RULE" < "$1"
+java -classpath $ANTLR_JAR:. org.antlr.v4.TestRig "$GRAMMAR" "$START_RULE" < "$1"
