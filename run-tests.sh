f37048cc92b44be393728e7be9e1ead1a13795ec
Federico Bond <federicobond@gmail.com>
2017-06-15 22:28:08 -0300

    Update test script to actually check for wrong tests

--- run-tests.sh
+++ run-tests.sh
@@ -5,13 +5,16 @@ ANTLR_JAR="antlr4.jar"
 GRAMMAR="Solidity"
 START_RULE="sourceUnit"
 TEST_FILE="test.sol"
-ERROR_PATTERN="(mismatched|extraneous)"
+ERROR_PATTERN="mismatched|extraneous"
 
 if [ ! -e "$ANTLR_JAR" ]; then
   curl http://www.antlr.org/download/antlr-4.7-complete.jar -o "$ANTLR_JAR"
 fi
 
-java -jar $ANTLR_JAR Solidity.g4
-javac -classpath $ANTLR_JAR $GRAMMAR*.java
+mkdir -p target/
 
-java -classpath $ANTLR_JAR:. org.antlr.v4.gui.TestRig "$GRAMMAR" "$START_RULE" < "$TEST_FILE"
+java -jar $ANTLR_JAR $GRAMMAR.g4 -o src/
+javac -classpath $ANTLR_JAR src/*.java -d target/
+
+java -classpath $ANTLR_JAR:target/ org.antlr.v4.gui.TestRig "$GRAMMAR" "$START_RULE" < "$TEST_FILE" 2>&1 |
+  grep -qE "$ERROR_PATTERN" && echo "TESTS FAIL!" || echo "TESTS PASS!"
