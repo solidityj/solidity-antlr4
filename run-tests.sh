610e50034765d34f1068305c8730e2921294724d
Federico Bond <federicobond@gmail.com>
2019-07-11 01:27:05 -0300

    Test with latest ANTLR tool

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,7 @@ TEST_FILE="test.sol"
 ERROR_PATTERN="mismatched|extraneous"
 
 if [ ! -e "$ANTLR_JAR" ]; then
-  curl https://www.antlr.org/download/antlr-4.7-complete.jar -o "$ANTLR_JAR"
+  curl https://www.antlr.org/download/antlr-4.7.2-complete.jar -o "$ANTLR_JAR"
 fi
 
 mkdir -p target/
